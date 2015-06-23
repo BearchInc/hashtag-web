f4421fb84f06eade77a5c555f28c430735593dde
Rafael Garcia <rafbgarcia@gmail.com>
2015-06-23 16:45:05 -0300

    [#2] Improve UI.

--- app/js/config/routes.js
+++ app/js/config/routes.js
@@ -1,38 +1,32 @@
 angular.module('Routes')
 
-  .config(['$stateProvider', '$urlRouterProvider',
-          function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise('/feed');
+.config(['$stateProvider', '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/feed');
 
-            function viewsWithMenu(bodyView) {
-              return {
-                menu: {
-                  templateUrl: '/public/views/menu.html',
-                  controller: 'MenuCtrl',
-                },
-                body: bodyView
-              };
-            }
+    function viewsWithMenu(bodyView) {
+      return {
+        menu: {
+          templateUrl: '/public/views/menu.html',
+          controller: 'MenuCtrl',
+        },
+        body: bodyView
+      };
+    }
 
-            $stateProvider
+    $stateProvider
 
-            .state('feed', {
-              url: '/feed',
-              views: viewsWithMenu({
-                templateUrl: '/public/views/feed.html',
-                controller: 'FeedCtrl',
-              }),
-            })
+    .state('feed', {
+      url: '/feed',
+      views: viewsWithMenu({
+        templateUrl: '/public/views/feed.html',
+        controller: 'FeedCtrl',
+      }),
+    })
 
-            .state('feed-deleted', {
-              url: '/feed-deleted',
-              views: viewsWithMenu({
-                templateUrl: '/public/views/feed_deleted.html',
-                controller: 'FeedDeletedCtrl',
-              }),
-            })
-          }
-  ])
-  .run(function ($rootScope, $state) {
-    $rootScope.$state = $state;
-  });
+  }
+])
+
+.run(function ($rootScope, $state) {
+  $rootScope.$state = $state;
+});
--- app/js/controllers/feed_controller.js
+++ app/js/controllers/feed_controller.js
@@ -1,38 +1,44 @@
 angular.module('Controllers')
 
-    .controller('FeedCtrl', function ($scope, $http, Post) {
-      $http.post(HOST + '/login', {id: 'hashtag-web'}).success(function(data, status) {
-        $http.defaults.headers.common.Authorization = 'Basic ' + btoa(data.account.auth_token);
+.controller('FeedCtrl', function ($scope, $http, $location) {
 
-        $http.get(HOST + '/posts').success(function(data, status) {
-            $scope.posts = data.posts;
-        });
-      });
+  $scope.getFeed = function (deleted) {
+    $scope.deleted = deleted;
 
-      $scope.deletePost = function (p) {
-        p.deleted = true, p.waiting = true;
-
-        $http.patch(HOST + p.delete_path)
-        .success(function(data, status) {
-          p.waiting = false;
-        })
-        .error(function () {
-          p.deleted = false, p.waiting = false;
-        });
-      };
-
-      $scope.restorePost = function (p) {
-        p.waiting = true;
-        p.deleted = false;
-
-        $http.delete(HOST + p.delete_path)
-        .success(function(data, status) {
-          p.waiting = false;
-        })
-        .error(function () {
-          p.deleted = true;
-          p.waiting = false;
-        });
-      };
+    $http.get(HOST + '/posts?deleted=' + deleted).success(function(data, status) {
+        $scope.posts = data.posts;
     });
+  }
+
+  $scope.deletePost = function (p) {
+    p.deleted = true, p.waiting = true;
+
+    $http.patch(HOST + p.path, {deleted: true})
+    .success(function(data, status) {
+      p.waiting = false;
+    })
+    .error(function () {
+      p.deleted = false, p.waiting = false;
+    });
+  };
+
+  $scope.restorePost = function (p) {
+    p.waiting = true; p.deleted = false;
+
+    $http.patch(HOST + p.path, {deleted: false})
+    .success(function(data, status) {
+      p.waiting = false;
+    })
+    .error(function () {
+      p.deleted = true; p.waiting = false;
+    });
+  };
+
+
+  $http.post(HOST + '/login', {id: 'hashtag-web'}).success(function(data, status) {
+    $http.defaults.headers.common.Authorization = 'Basic ' + btoa(data.account.auth_token);
+    $scope.getFeed($location.search().deleted);
+  });
+
+});
 
