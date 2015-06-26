2329842c2739026132b15379476c0e89f1236681
Rafael Garcia <rafbgarcia@gmail.com>
2015-06-26 15:06:29 -0300

    [#9] Merge with tags story.

--- app/js/controllers/posts_controller.js
+++ app/js/controllers/posts_controller.js
@@ -1,14 +1,11 @@
 angular.module('Controllers')
 
-.controller('PostsIndex', function ($scope, $http, $modal, Post, Flash) {
+.controller('PostsIndex', function ($scope, $http, $modal, $sce, $location, Post, Flash) {
   $scope.allPosts = function () {
     $scope.posts = [];
     $scope.deletedActive = false;
 
-    Post.all()
-      .success(function(data, status) {
-        $scope.posts = data.posts;
-      });
+    Post.all().success(handlePosts);
   };
 
   $scope.deletedPosts = function () {
@@ -16,15 +13,51 @@ angular.module('Controllers')
     $scope.deletedActive = true;
 
     Post.deleted()
-      .success(function(data, status) {
-        $scope.posts = data.posts.map(function (p) {
-          p.deleted = true;
-          return p;
-        });
-      })
+        .success(function(data, status) {
+          data.posts = data.posts.map(function (p) {
+            p.deleted = true;
+            return p;
+          });
+          handlePosts(data)
+        })
   };
 
   $scope.allPosts();
+
+
+  $scope.getTagFeed = function (tag) {
+    $scope.posts = [];
+
+    $http.get(HOST + '/feeds/' + tag).success(handlePosts);
+  };
+
+  function handlePosts (data) {
+    var posts = data.posts;
+    var tagregex = /#([a-zA-Z0-9_-]+)($|[ !.?,;:])/g;
+
+    posts.forEach(function(p, index, posts) {
+      posts[index].description = $sce.trustAsHtml(p.description.replace(tagregex, function(match, tag) {
+        return '<a href="/#/posts?tag=' + tag + '">' + match + '</a>';
+      }));
+    });
+
+    $scope.posts = posts;
+  }
+
+  function loadFeed() {
+    var params = $location.search();
+    if (params.tag) {
+      $scope.getTagFeed(params.tag);
+    } else {
+      $scope.allPosts();
+    }
+  }
+
+  $scope.$on('$locationChangeStart', loadFeed);
+
+  loadFeed();
+
+
 
 
   $scope.deletePost = function (p) {
@@ -73,7 +106,7 @@ angular.module('Controllers')
           Flash.create('danger', 'Err: ' + data);
           return;
         }
-        Flash.create('success', 'Account successfully blocked');
+        Flash.create('success', 'Author successfully blocked');
       })
       .error(function () {
         Flash.create('danger', 'Err: ' + data);
